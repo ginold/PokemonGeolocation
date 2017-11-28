ea5ea8ad4fd315d71bbab2a4a2d988d4b270b2c5
Adam Gina <dm171521@fhstp.ac.at>
2017-11-28 20:18:06 +0100

    add 'Add' path + names to the router

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,16 @@
 import * as PokemonMap from '../components/Map'
 import List from '../components/List'
 import NotFound from '../components/NotFound'
-
+import Add from '../components/Add'
 import VueRouter from 'vue-router'
 
 // can be a JS file, just write 'export'
 export const router = new VueRouter({
   routes: [
-    { path: '/', component: PokemonMap },
-    { path: '/map', component: PokemonMap },
-    { path: '/list', component: List },
+    { path: '/add', component: Add, name: 'add' },
+    { path: '/', component: PokemonMap, name: 'map' },
+    { path: '/map', component: PokemonMap, name: 'map' },
+    { path: '/list', component: List, name: 'list' },
     { path: '*', component: NotFound } // * for not found routes
   ]
 })
