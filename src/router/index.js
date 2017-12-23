e7743f3f720d18a96903e675e37b1103c6cbb062
Adam Gina <dm171521@fhstp.ac.at>
2017-12-23 13:50:38 +0100

    add navigation guards

--- src/router/index.js
+++ src/router/index.js
@@ -4,18 +4,26 @@ import NotFound from '../components/NotFound'
 import Add from '../components/Add'
 import Login from '../components/Login'
 import Register from '../components/Register'
-
 import VueRouter from 'vue-router'
+import store from '../store'
 
 // can be a JS file, just write 'export'
 export const router = new VueRouter({
   routes: [
-    { path: '/register', component: Register, name: 'register' },
-    { path: '/login', component: Login, name: 'login' },
-    { path: '/add', component: Add, name: 'add' },
+    { path: '/register', component: Register, name: 'register', meta: {requires: 'visitor'} },
+    { path: '/login', component: Login, name: 'login', meta: {requires: 'visitor'} },
+    { path: '/add', component: Add, name: 'add', meta: {requires: 'auth'} },
     { path: '/', component: PokemonMap, name: 'home' },
-    { path: '/map', component: PokemonMap, name: 'map' },
-    { path: '/list', component: List, name: 'list' },
+    { path: '/map', component: PokemonMap, name: 'map', meta: {requires: 'visitor'} },
+    { path: '/list', component: List, name: 'list', meta: {requires: 'auth'} },
     { path: '*', component: NotFound } // * for not found routes
   ]
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requires === 'auth')) {
+    if (!store.getters.getAuthtoken) next('/login')
+  } else {
+    next()
+  }
 })
